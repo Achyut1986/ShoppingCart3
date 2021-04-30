d5a1dbbef0234052362d958bd7366e71a5c97dfa
Achyut1986 <82679069+Achyut1986@users.noreply.github.com>
2021-04-30 09:26:48 -0500

    test test

--- src/test/java/com/cognizant/ShoppingCartTest.java
+++ src/test/java/com/cognizant/ShoppingCartTest.java
@@ -21,5 +21,27 @@ public class ShoppingCartTest {
         Item item = new Item(80.9, "Apple");
         assertEquals(80.9, cart.getSubTotal(), 80.9);
     }
+    @Test
+    void sumOfTotalAddingMoreItems(){
+        List<Item>itemList = new ArrayList<>();
+        Cart cart = new Cart(itemList);
+
+        Item item1 = new Item(80.9, "Apple");
+        Item item2 = new Item(100, "Orange");
+
+        assertEquals(180.9, cart.getSubTotal(), 180.9);
+    }
+    @Test
+    void sumOfQuantityOfSameItem(){
+        List<Item>itemList= new ArrayList<>();
+        Cart cart = new Cart(itemList);
+
+        Item item1 = new Item(80.9, "Apple");
+        Item item2 = new Item(80.9, "Apple");
+        Item item3 = new Item(100, "Orange");
+        Item item4 = new Item(100, "Orange");
+
+        assertEquals(561.8, cart.getSubTotal(),561.8);
+    }
 
 }
